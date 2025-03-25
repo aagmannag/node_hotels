921811ded034e8effd881beb7b4364b95f0b7088
aagmannag <aagmannag@gmail.com>
2025-03-25 14:24:16 +0530

    shift mongodb to online server

--- db.js
+++ db.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-const mongoURL = 'mongodb://127.0.0.1:27017/hotels';
+// const mongoURL = 'mongodb://127.0.0.1:27017/hotels';
+// const mondoURL = process.env.MONGODB_URL_Local;
 
+// const mongoURL = 'mongodb+srv://admin:AAGMan123@cluster0.8vwo0.mongodb.net/'; 
+const mongoURL = process.env.MONGODB_URL;
 
 mongoose.connect(mongoURL);
 
@@ -13,7 +17,7 @@ db.on('connected', () => {
 
 db.on('error', (err) => {
     console.log('Error connecting to MongoDB', err);
-});
+}); 
 
 db.on('disconnected', () => {   
     console.log('Disconnected from MongoDB');
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const db = require("./db"); // make sure db.js is in the same folder
+require('dotenv').config();
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json()); // req.body
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World");
@@ -14,6 +16,7 @@ const menuItemRoutes = require("./routes/menuItemRoutes");
 
 app.use('/person', personRoutes);
 app.use('/menu', menuItemRoutes);
+
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
